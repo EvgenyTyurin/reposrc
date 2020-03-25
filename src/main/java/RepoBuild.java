5398ff56113324fbf281e082986f4d16b22cdb8a
EUTyrin <e_tyrin@eureca.ru>
2020-03-25 16:05:39 +0300

    update

--- src/main/java/RepoBuild.java
+++ src/main/java/RepoBuild.java
@@ -1,9 +1,25 @@
 import java.io.IOException;
+import java.util.List;
 
 public class RepoBuild {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        CmdExec.exec("test.bat");
+        boolean buildResult =
+                build("D:\\projects\\parsec_repo2\\batik\\batik-awt-util\\1.6-1");
+    }
+
+    private static boolean build(String dir) throws IOException, InterruptedException {
+        List<String> buildResult =
+                CmdExec.exec("compile1.bat " + dir);
+        for (String str : buildResult) {
+            if (str.contains("BUILD FAILURE")) {
+                return false;
+            }
+            if (str.contains("BUILD SUCCESS")) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
